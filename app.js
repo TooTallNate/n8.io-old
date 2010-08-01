bd3644d2bbe2059fb9a8eb9be64818f0b4321585
Nathan Rajlich <nathan@tootallnate.net>
2010-08-01 12:05:36 -0700

    Add wheat as a submodule

---
+++ app.js
@@ -0,0 +1,11 @@
+// Just a basic server setup for this site
+var Connect = require('connect');
+
+module.exports = Connect.createServer(
+  Connect.responseTime(),
+  Connect.logger(),
+  Connect.conditionalGet(),
+  Connect.cache(),
+  Connect.gzip(),
+  require('./wheat/lib/wheat')(__dirname)
+);
