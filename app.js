65588a24f4ba4121df7a0f81b5fe4d4fd79e74d7
Nathan Rajlich <nathan@tootallnate.net>
2011-11-09 09:51:16 -0800

    Call listen() on the dev server if not using `spark`.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Just a basic development setup for the blog...
 var Connect = require('connect');
-module.exports = Connect.createServer(
+var server = module.exports = Connect.createServer(
   Connect.logger(),
   require('wheat')(__dirname)
-);
+)
+if (!module.parent) {
+  server.listen(3000, function () {
+    console.log('TooTallNate.net dev server listening:', this.address());
+  });
+}
