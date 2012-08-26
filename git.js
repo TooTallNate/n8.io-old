885f2b89e74e9ed81295836a9db0a6b71f96bc7b
Nathan Rajlich <nathan@tootallnate.net>
2012-08-26 16:42:03 -0700

    new blog progress point

---
+++ git.js
@@ -0,0 +1,72 @@
+
+var ffi = require('ffi');
+var ref = require('ref');
+var Struct = require('ref-struct');
+
+// types
+var git_commit = 'void *';
+var git_oid = 'void *';
+var git_object = 'void *';
+var git_reference = 'void *';
+var git_repository = 'void *';
+var git_time_t = 'int64';
+var git_time = Struct({
+  time: git_time_t,
+  offset: 'int'
+});
+var git_signature = Struct({
+  name: 'string',
+  email: 'string',
+  when: git_time
+});
+exports.git_commit = git_commit;
+exports.git_oid = git_oid;
+exports.git_object = git_object;
+exports.git_reference = git_reference;
+exports.git_repository = git_repository;
+exports.git_time_t = git_time_t;
+exports.git_time = git_time;
+exports.git_signature = git_signature;
+
+
+var git_otype = 'int'; // enum
+var GIT_OBJ_COMMIT = 1;
+
+
+// libffi bindings
+ffi.Library('libgit2', {
+  git_commit_message_encoding: [ 'string', [ git_commit ] ],
+  git_commit_message: [ 'string', [ git_commit ] ],
+  git_commit_time: [ git_time_t, [ git_commit ] ],
+  git_commit_time_offset: [ 'int', [ git_commit ] ],
+  git_commit_committer: [ ref.refType(git_signature), [ git_commit ] ],
+  git_commit_author: [ ref.refType(git_signature), [ git_commit ] ],
+  git_commit_parentcount: [ 'uint', [ git_commit ] ],
+  git_commit_parent: [ 'int', [ ref.refType(git_commit), git_commit, 'uint' ] ],
+
+  git_object_free: [ 'void', [ git_object ] ],
+  git_object_lookup: [ 'int', [ ref.refType(git_object), git_repository, git_oid, git_otype ] ],
+  git_object_type: [ git_otype, [ git_object ] ],
+  git_object_type2string: [ 'string', [ git_otype ] ],
+  git_object_string2type: [ git_otype, [ 'string' ] ],
+  git_object_typeisloose: [ 'int', [ git_otype ] ],
+  git_object__size: [ 'size_t', [ git_otype ] ],
+
+  git_oid_tostr: [ 'string', [ 'char*', 'size_t', git_oid ] ],
+
+  git_reference_name: [ 'string', [ git_reference ] ],
+  git_reference_oid: [ git_oid, [ git_reference ] ],
+  git_reference_target: [ 'string', [ git_reference ] ],
+  git_reference_type: [ 'int', [ git_reference ] ],
+
+  git_repository_open: [ 'int', [ ref.refType(git_repository), 'string' ] ],
+  git_repository_head: [ 'int', [ ref.refType(git_reference), git_repository ] ],
+}, exports);
+
+
+// inline functions
+exports.git_commit_lookup = function (commit, repo, id) {
+  return exports.git_object_lookup(commit, repo, id, GIT_OBJ_COMMIT);
+};
+
+exports.git_commit_free = exports.git_object_free;
--- server.js
+++ server.js
@@ -1,18 +1,107 @@
+var url = require('url');
+var ref = require('ref');
+var git = require('./git');
 var express = require('express');
 
 var app = module.exports = express();
+var repo_path = __dirname + '/.git';
+var err;
+
+
+// first get the "git_repository" instance for this repo
+var repo = ref.alloc(ref.refType(git.git_repository));
+err = git.git_repository_open(repo, repo_path);
+if (err !== 0) {
+  throw new Error('git_repository_open: error opening');
+}
+repo = repo.deref();
+
+
+/**
+ * Connect logger.
+ */
 
 app.use(express.logger());
-app.use(function (req, res, next) {
-  res.end('hello\n');
+
+
+/**
+ * When an SHA commit was specified.
+ */
+
+app.get(/^\/([\da-f]+)\/?/, function (req, res, next) {
+  var sha = req.params[0];
+  req.sha = sha;
+  var origUrl = req.url;
+  req.url = req.url.substring(sha.length + 1);
+  if (!req.path) {
+    // only an SHA, no trailing "/" or anything else, redirect to "/"
+    var parsed = url.parse(origUrl);
+    return res.redirect('/' + sha + '/' + (parsed.query ? '?' + parsed.query : ''));
+  }
+  next();
 });
 
+
+/**
+ * Resolve HEAD if no SHA was specified.
+ */
+
+app.get('*', function (req, res, next) {
+  if (req.sha) return next();
+  // we must populate "req.sha" with the sha of HEAD
+  // TODO: cache probably...
+  var buf = new Buffer(41); // SHA + \0 byte
+  var head = ref.alloc(ref.refType(git.git_repository));
+  git.git_repository_head.async(head, repo, onHead);
+  function onHead (err, rtn) {
+    if (err) return next(err); // ffi error
+    if (rtn !== 0) return next(new Error('git_repository_head: error ' + rtn)); // libgit2 error
+    head = head.deref();
+    git.git_reference_oid.async(head, onOid);
+  }
+  function onOid (err, head_oid) {
+    if (err) return next(err); // ffi error
+    git.git_oid_tostr.async(buf, buf.length, head_oid, onSha);
+  }
+  function onSha (err, sha) {
+    if (err) return next(err); // ffi error
+    req.sha = sha;
+    next(); // done
+  }
+});
+
+
+/**
+ * Resolve the short SHA if a short one was given.
+ */
+
+//app.get('*', function (req, res, next) {
+//  var sha = req.sha;
+
+
+/**
+ * Serve the specified path from the specified SHA commit.
+ */
+
+app.get('*', function (req, res, next) {
+  var sha = req.sha;
+  if ('string' != typeof sha) return next(new Error('No SHA. This SHOULD NOT HAPPEN!'));
+  if (40 != sha.length) return next(new Error('SHA length is invalid: ' + sha.length));
+  var path = req.path;
+  res.end("Serving \"" + path + "\" from commit " + sha + "\n");
+});
+
+
+/**
+ * Listen?
+ */
 
 if (!module.parent) {
   // If this module is being invoked directly, then
   // fire up the server on the given port.
+  var http = require('http');
   var port = parseInt(process.env.N8_IO_PORT, 10) || 3000;
-  app.listen(port, function () {
+  http.createServer(app).listen(port, function () {
     console.log('TooTallNate.net dev server listening:', this.address());
   });
 }
