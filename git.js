08355d502432d9081a3308529e8a004a008157c8
Nathan Rajlich <nathan@tootallnate.net>
2012-08-27 10:34:25 -0700

    git: define git_oid as the Struct type that it is

--- git.js
+++ git.js
@@ -6,13 +6,35 @@ var Struct = require('ref-struct');
 // types
 var git_blob = 'void *';
 var git_commit = 'void *';
-var git_oid = 'void *';
 var git_object = 'void *';
 var git_reference = 'void *';
 var git_repository = 'void *';
 var git_tree = 'void *';
 var git_tree_entry = 'void *';
 var git_time_t = 'int64';
+var git_oid = Struct({
+  // TODO: use ref-array here...
+  id0: 'uchar',
+  id1: 'uchar',
+  id2: 'uchar',
+  id3: 'uchar',
+  id4: 'uchar',
+  id5: 'uchar',
+  id6: 'uchar',
+  id7: 'uchar',
+  id8: 'uchar',
+  id9: 'uchar',
+  id10: 'uchar',
+  id11: 'uchar',
+  id12: 'uchar',
+  id13: 'uchar',
+  id14: 'uchar',
+  id15: 'uchar',
+  id16: 'uchar',
+  id17: 'uchar',
+  id18: 'uchar',
+  id19: 'uchar'
+});
 var git_time = Struct({
   time: git_time_t,
   offset: 'int'
@@ -43,6 +65,7 @@ exports.git_tree_entry = git_tree_entry;
 var git_otype = 'int'; // enum
 var GIT_OBJ_COMMIT = 1;
 
+var git_oid_ptr = ref.refType(git_oid);
 
 // libffi bindings
 ffi.Library('libgit2', {
@@ -55,42 +78,42 @@ ffi.Library('libgit2', {
   git_commit_time: [ git_time_t, [ git_commit ] ],
   git_commit_time_offset: [ 'int', [ git_commit ] ],
   git_commit_committer: [ ref.refType(git_signature), [ git_commit ] ],
-  git_commit_id: [ git_oid, [ git_commit ] ],
+  git_commit_id: [ git_oid_ptr, [ git_commit ] ],
   git_commit_author: [ ref.refType(git_signature), [ git_commit ] ],
   git_commit_parentcount: [ 'uint', [ git_commit ] ],
   git_commit_parent: [ 'int', [ ref.refType(git_commit), git_commit, 'uint' ] ],
   git_commit_tree: [ 'int', [ ref.refType(git_tree), git_commit ] ],
 
   git_object_free: [ 'void', [ git_object ] ],
-  git_object_lookup: [ 'int', [ ref.refType(git_object), git_repository, git_oid, git_otype ] ],
-  git_object_lookup_prefix: [ 'int', [ ref.refType(git_object), git_repository, git_oid, 'uint', git_otype ] ],
+  git_object_lookup: [ 'int', [ ref.refType(git_object), git_repository, git_oid_ptr, git_otype ] ],
+  git_object_lookup_prefix: [ 'int', [ ref.refType(git_object), git_repository, git_oid_ptr, 'uint', git_otype ] ],
   git_object_type: [ git_otype, [ git_object ] ],
   git_object_type2string: [ 'string', [ git_otype ] ],
   git_object_string2type: [ git_otype, [ 'string' ] ],
   git_object_typeisloose: [ 'int', [ git_otype ] ],
   git_object__size: [ 'size_t', [ git_otype ] ],
 
-  git_oid_fmt: [ 'void', [ 'char*', git_oid ] ],
-  git_oid_fromstr: [ 'int', [ git_oid, 'string' ] ],
-  git_oid_fromstrn: [ 'int', [ git_oid, 'string', 'size_t' ] ],
-  git_oid_tostr: [ 'string', [ 'char*', 'size_t', git_oid ] ],
+  git_oid_fmt: [ 'void', [ 'char*', git_oid_ptr ] ],
+  git_oid_fromstr: [ 'int', [ git_oid_ptr, 'string' ] ],
+  git_oid_fromstrn: [ 'int', [ git_oid_ptr, 'string', 'size_t' ] ],
+  git_oid_tostr: [ 'string', [ 'char*', 'size_t', git_oid_ptr ] ],
 
   git_reference_name: [ 'string', [ git_reference ] ],
-  git_reference_oid: [ git_oid, [ git_reference ] ],
+  git_reference_oid: [ git_oid_ptr, [ git_reference ] ],
   git_reference_target: [ 'string', [ git_reference ] ],
   git_reference_type: [ 'int', [ git_reference ] ],
 
   git_repository_open: [ 'int', [ ref.refType(git_repository), 'string' ] ],
   git_repository_head: [ 'int', [ ref.refType(git_reference), git_repository ] ],
 
-  git_tree_id: [ git_oid, [ git_tree ] ],
+  git_tree_id: [ git_oid_ptr, [ git_tree ] ],
   git_tree_entrycount: [ 'uint', [ git_tree ] ],
   git_tree_entry_byname: [ git_tree_entry, [ git_tree, 'string' ] ],
   git_tree_entry_byindex: [ git_tree_entry, [ git_tree, 'uint' ] ],
 
   git_tree_entry_attributes: [ 'uint', [ git_tree_entry ] ],
   git_tree_entry_name: [ 'string', [ git_tree_entry ] ],
-  git_tree_entry_id: [ git_oid, [ git_tree_entry ] ],
+  git_tree_entry_id: [ git_oid_ptr, [ git_tree_entry ] ],
   git_tree_entry_type: [ git_otype, [ git_tree_entry ] ],
   git_tree_entry_to_object: [ 'int', [ ref.refType(git_object), git_repository, git_tree_entry ] ],
   git_tree_get_subtree: [ 'int', [ ref.refType(git_tree), git_tree, 'string' ] ],
