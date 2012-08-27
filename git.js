62486192cb9aaad86090be2fec54fffa4d8e971e
Nathan Rajlich <nathan@tootallnate.net>
2012-08-26 17:50:45 -0700

    another progress point...

--- git.js
+++ git.js
@@ -40,18 +40,23 @@ ffi.Library('libgit2', {
   git_commit_time: [ git_time_t, [ git_commit ] ],
   git_commit_time_offset: [ 'int', [ git_commit ] ],
   git_commit_committer: [ ref.refType(git_signature), [ git_commit ] ],
+  git_commit_id: [ git_oid, [ git_commit ] ],
   git_commit_author: [ ref.refType(git_signature), [ git_commit ] ],
   git_commit_parentcount: [ 'uint', [ git_commit ] ],
   git_commit_parent: [ 'int', [ ref.refType(git_commit), git_commit, 'uint' ] ],
 
   git_object_free: [ 'void', [ git_object ] ],
   git_object_lookup: [ 'int', [ ref.refType(git_object), git_repository, git_oid, git_otype ] ],
+  git_object_lookup_prefix: [ 'int', [ ref.refType(git_object), git_repository, git_oid, 'uint', git_otype ] ],
   git_object_type: [ git_otype, [ git_object ] ],
   git_object_type2string: [ 'string', [ git_otype ] ],
   git_object_string2type: [ git_otype, [ 'string' ] ],
   git_object_typeisloose: [ 'int', [ git_otype ] ],
   git_object__size: [ 'size_t', [ git_otype ] ],
 
+  git_oid_fmt: [ 'void', [ 'char*', git_oid ] ],
+  git_oid_fromstr: [ 'int', [ git_oid, 'string' ] ],
+  git_oid_fromstrn: [ 'int', [ git_oid, 'string', 'size_t' ] ],
   git_oid_tostr: [ 'string', [ 'char*', 'size_t', git_oid ] ],
 
   git_reference_name: [ 'string', [ git_reference ] ],
@@ -67,6 +72,10 @@ ffi.Library('libgit2', {
 // inline functions
 exports.git_commit_lookup = function (commit, repo, id) {
   return exports.git_object_lookup(commit, repo, id, GIT_OBJ_COMMIT);
+};
+
+exports.git_commit_lookup_prefix = function (commit, repo, id, len) {
+  return exports.git_object_lookup_prefix(commit, repo, id, len, GIT_OBJ_COMMIT);
 };
 
 exports.git_commit_free = exports.git_object_free;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var app = module.exports = express();
 var repo_path = __dirname + '/.git';
 var err;
 
+const OID_RAWSZ = 20;
+const OID_HEXSZ = OID_RAWSZ * 2;
+
 
 // first get the "git_repository" instance for this repo
 var repo = ref.alloc(ref.refType(git.git_repository));
@@ -28,7 +31,7 @@ app.use(express.logger());
  * When an SHA commit was specified.
  */
 
-app.get(/^\/([\da-f]+)\/?/, function (req, res, next) {
+app.get(/^\/[0-9a-f]{5,40}\b/, function (req, res, next) {
   var sha = req.params[0];
   req.sha = sha;
   var origUrl = req.url;
@@ -50,7 +53,7 @@ app.get('*', function (req, res, next) {
   if (req.sha) return next();
   // we must populate "req.sha" with the sha of HEAD
   // TODO: cache probably...
-  var buf = new Buffer(41); // SHA + \0 byte
+  var buf; // SHA
   var head = ref.alloc(ref.refType(git.git_repository));
   git.git_repository_head.async(head, repo, onHead);
   function onHead (err, rtn) {
@@ -61,11 +64,12 @@ app.get('*', function (req, res, next) {
   }
   function onOid (err, head_oid) {
     if (err) return next(err); // ffi error
-    git.git_oid_tostr.async(buf, buf.length, head_oid, onSha);
+    buf = new Buffer(OID_HEXSZ);
+    git.git_oid_fmt.async(buf, head_oid, onSha);
   }
-  function onSha (err, sha) {
+  function onSha (err) {
     if (err) return next(err); // ffi error
-    req.sha = sha;
+    req.sha = buf.toString('ascii');
     next(); // done
   }
 });
@@ -75,8 +79,39 @@ app.get('*', function (req, res, next) {
  * Resolve the short SHA if a short one was given.
  */
 
-//app.get('*', function (req, res, next) {
-//  var sha = req.sha;
+app.get('*', function (req, res, next) {
+  var sha = req.sha;
+  if ('string' != typeof sha) return next(new Error('No SHA. This SHOULD NOT HAPPEN!'));
+  if (OID_HEXSZ == sha.length) return next(); // SHA is good already
+  // need to resolve the short SHA and then redirect
+  var short_oid = ref.alloc(git.git_oid);
+  var commit, buf, oid;
+  git.git_oid_fromstrn.async(short_oid, sha, sha.length, onOid);
+  function onOid (err, rtn) {
+    if (err) return next(err); // ffi error
+    if (rtn !== 0) return next(new Error('git_oid_fromstrn: error ' + rtn)); // libgit2 error
+    commit = ref.alloc(ref.refType(git.git_commit));
+    // TODO: use inline wrapper
+    git.git_object_lookup_prefix.async(commit, repo, short_oid, sha.length, 1, onCommit);
+  }
+  function onCommit (err, rtn) {
+    if (err) return next(err); // ffi error
+    if (rtn !== 0) return next(new Error('git_commit_lookup_prefix: error ' + rtn)); // libgit2 error
+    commit = commit.deref();
+    git.git_commit_id.async(commit, onCommitId);
+  }
+  function onCommitId (err, _oid) {
+    if (err) return next(err); // ffi error
+    buf = new Buffer(OID_HEXSZ);
+    oid = _oid;
+    git.git_oid_fmt.async(buf, oid, onSha);
+  }
+  function onSha (err) {
+    if (err) return next(err); // ffi error
+    var full_sha = buf.toString('ascii');
+    res.redirect('/' + full_sha + req.url);
+  }
+});
 
 
 /**
@@ -85,8 +120,6 @@ app.get('*', function (req, res, next) {
 
 app.get('*', function (req, res, next) {
   var sha = req.sha;
-  if ('string' != typeof sha) return next(new Error('No SHA. This SHOULD NOT HAPPEN!'));
-  if (40 != sha.length) return next(new Error('SHA length is invalid: ' + sha.length));
   var path = req.path;
   res.end("Serving \"" + path + "\" from commit " + sha + "\n");
 });
