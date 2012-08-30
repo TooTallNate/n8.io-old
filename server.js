b74ab8085a8f4f74bc9da454bb8c189e8dcb79f9
Nathan Rajlich <nathan@tootallnate.net>
2012-08-30 12:04:18 -0700

    semicolon

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var app = require('./app')
+var app = require('./app');
 var port = parseInt(process.env.N8_IO_PORT, 10) || 3000;
 http.createServer(app).listen(port, function () {
   console.log('TooTallNate.net dev server listening:', this.address());
