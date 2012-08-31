1d53d34c6afdfe5b2f2060de3db692a49027a1be
Nathan Rajlich <nathan@tootallnate.net>
2012-08-30 18:47:18 -0700

    comments

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
 var http = require('http');
 var app = require('./app');
 var port = parseInt(process.env.N8_IO_PORT, 10) || 3000;
+
+/**
+ * Create server and listen.
+ */
+
 http.createServer(app).listen(port, function () {
   console.log('TooTallNate.net dev server listening:', this.address());
 });
