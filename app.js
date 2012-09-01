88322f8b178e65dd700ab56eda3e524446a7fce6
Nathan Rajlich <nathan@tootallnate.net>
2012-09-01 13:45:44 -0700

    make the homepage work again
    This is essentially the end of the rewrite :)

--- app.js
+++ app.js
@@ -61,148 +61,15 @@ app.get('*', require('./lib/head'));
 // by now `req.sha` is guaranteed to be set
 app.get('*', require('./lib/root-tree'));
 
+// `req.article_names` is used by every request
+app.get('*', require('./lib/article-names'));
+
 // by now `req.root_tree` is a "git_tree" instance to the resolved SHA
 app.get('/', require('./lib/homepage'));
 app.get('/articles', require('./lib/articles'));
 
 // attempt to render an article if this a request for one
-app.get('*', require('./lib/article-names'));
 app.get('*', require('./lib/article'));
 
 // finally attempt to serve static files from the public/ dir
 app.get('*', require('./lib/static'));
-
-
-/**
- * Sort 2 objects by their "date" properties.
- */
-
-function by_date (a, b) {
-  var da = new Date(a.date);
-  var db = new Date(b.date);
-  return db - da;
-}
-
-
-/**
- * Render the homepage.
- */
-
-/*
-app.get('/', articles, file('views/layout.jade'), file('views/index.jade'),
-compile('views/layout.jade'), compile('views/index.jade'),
-function (req, res, next) {
-  debug('rendering "/" route');
-  var layout = req.templates['views/layout.jade'];
-  var index = req.templates['views/index.jade'];
-
-  // TODO: consolidate this rendering logic with the articles page
-  var locals = {};
-  locals.months = months;
-  locals.sha = req.sha;
-  locals.versions = process.versions;
-  locals.articles = req.articles.sort(by_date);
-  locals.avatar = gravatar('nathan@tootallnate.net', { s: 500 });
-
-  // render the index template
-  locals.body = index(locals);
-
-  // render and send the layout template
-  res.type('html');
-  res.send(layout(locals));
-  debug('sending "/" route result HTML');
-});
-*/
-
-
-/**
- * Render an article.
- * TODO: don't use "articles", just get the valid names
- */
-
-/*
-app.get('*', articles, file('views/article.jade'), file('views/layout.jade'),
-compile('views/article.jade'), compile('views/layout.jade'),
-function (req, res, next) {
-  var valid = req.articles.map(function (a) { return a.name; });
-  var name = req.path.substring(1);
-  if (!~valid.indexOf(name)) return next();
-  debug('rendering article "%s" route', name);
-
-  var layout = req.templates['views/layout.jade'];
-  var article = req.templates['views/article.jade'];
-  var locals = {};
-  locals.months = months;
-  locals.sha = req.sha;
-  locals.versions = process.versions;
-  locals.avatar = gravatar('nathan@tootallnate.net', { s: 500 });
-  locals.article = req.articles.filter(function (a) { return a.name == name })[0];
-
-  // render the article template
-  locals.body = article(locals);
-
-  // render and send the layout template
-  res.type('html');
-  res.send(layout(locals));
-  debug('sending "/%s" route result HTML');
-});
-*/
-
-
-/**
- * Populates "req.articles" with an Array the names of the "articles" for the
- * current commit.
- */
-
-function articles (req, res, next) {
-  debug('populating "req.articles"');
-  var root = req.root_tree;
-  var articles;
-  git.git_tree_entry_byname.async(root, 'articles', onEntry);
-  function onEntry (err, articles_entry) {
-    if (err) return next(err); // ffi error
-    if (articles_entry.isNull()) return next(new Error('"articles" dir does not exist for this commit'));
-    articles = ref.alloc(ref.refType(git.git_tree));
-    git.git_tree_entry_to_object.async(articles, repo, articles_entry, onTree);
-  }
-  function onTree (err, rtn) {
-    if (err) return next(err); // ffi error
-    if (rtn !== 0) return next(new Error('git_tree_entry_to_object: error ' + rtn)); // libgit2 error
-    articles = articles.deref();
-
-    // TODO: make async, I'm being lazy ATM
-    var count = git.git_tree_entrycount(articles);
-    req.articles = [];
-    for (var i = 0; i < count; i++) {
-      var entry = git.git_tree_entry_byindex(articles, i);
-      var name = git.git_tree_entry_name(entry);
-      var is_article = path.extname(name) == '.markdown';
-      if (is_article) {
-        debug('populating "req.articles[%d]" with %j', req.articles.length, name);
-        var article = {};
-        req.articles.push(article);
-        article.filename = name;
-        article.name = name.replace(/\.markdown$/, '');
-        article.raw = entry_to_buffer(entry).toString('utf8');
-        var split = article.raw.indexOf('\n\n');
-        var headers = article.raw.substring(0, split).split('\n');
-        article.headers = headers;
-        headers.forEach(function (h) {
-          var split = h.indexOf(':');
-          var name = h.substring(0, split);
-          if (h[split + 1] == ' ') split++;
-          var val = h.substring(split + 1);
-          article[name.toLowerCase()] = val;
-        });
-        article.html = markdown(article.raw.substring(split));
-
-        // the first paragraph
-        article.desc = article.html.substring(0, article.html.indexOf('</p>'));
-      } else {
-        debug('skipping %j since it doesn\'t end with ".markdown"', name);
-      }
-    }
-    debug('done populating "req.articles"');
-    next();
-  }
-}
