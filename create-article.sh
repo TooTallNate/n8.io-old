069d982e53efb11b476f2860862641e34cb6ce7d
Nathan Rajlich <nathan@tootallnate.net>
2012-09-03 13:18:57 -0700

    create-article: automatically jump into the editor

--- create-article.sh
+++ create-article.sh
@@ -9,3 +9,10 @@ DIR=`dirname $0`
 FILE="$DIR/articles/$SLUG.markdown"
 echo "Title: $TITLE" > "$FILE"
 echo "Date: `node -pe "new Date().toUTCString()"`" >> "$FILE"
+echo >> "$FILE"
+echo >> "$FILE"
+
+# edit
+if [ "x$EDITOR" != "x" ]; then
+  "$EDITOR" "$FILE"
+fi
