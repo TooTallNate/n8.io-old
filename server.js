5a5a33af2305f1eb68ffbe1f4bdf10f59c3b86d4
Nathan Rajlich <nathan@tootallnate.net>
2015-06-15 18:40:47 -0700

    server: add the bound port number to the process title

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ var app = require('./app');
 var port = parseInt(process.env.N8_IO_PORT, 10) || 3000;
 
 /**
- * Set the process title.
- */
-
-process.title = 'n8.io';
-
-/**
  * Create HTTP server.
  */
 
@@ -25,6 +19,10 @@ var server = http.createServer(app);
 
 server.listen(port, function () {
   port = server.address().port;
+
+ // Set the process title.
+  process.title = 'n8.io: port ' + port;
+
   console.log('n8.io %j server listening on port: %d', app.settings.env, port);
   console.log('libgit2 version %j', app.settings.version);
 });
