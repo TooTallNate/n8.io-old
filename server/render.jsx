67f461d39be78d13dc127d616fa41694edb6a288
Nathan Rajlich <nathan@tootallnate.net>
2016-04-16 11:52:06 -0700

    server: add `avatar` and `sha` to the render call

--- server/render.jsx
+++ server/render.jsx
@@ -1,9 +1,14 @@
+import sha from '../sha';
 import render from '../layout';
+import { url as gravatar } from 'gravatar';
 import DEBUG from 'debug';
 
 const debug = DEBUG('n8.io:render');
 
 export default async function (req, res) {
+  const avatar = gravatar('nathan@tootallnate.net', { s: 500 });
+  const locals = { sha, avatar };
+
   res.setHeader('Content-Type', 'text/html; charset=utf8');
-  return render({ sha: '' });
+  return render(locals);
 }
