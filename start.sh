59177a212dabbdd8c4d1517166a0cbde0353f62f
Nathan Rajlich <nathan@tootallnate.net>
2016-04-16 22:54:27 -0700

    remove all the old git-related logic
    Losing the git functionality here, but that was buggy,
    half-baked and never fully completed anyways.

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This is an wasy way to start the server in the background.
-# Meant for use in production on a linux server.
-export LD_LIBRARY_PATH=/root/libgit2/build
-export NODE_ENV=production
-export N8_IO_PORT=3000
-node server.js extra args needed for process title </dev/null >/dev/null 2>&1 &
