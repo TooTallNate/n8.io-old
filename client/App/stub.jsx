8749790963ddab1ea1e33dd6343ea7aa87b7664d
Nathan Rajlich <nathan@tootallnate.net>
2016-04-17 19:09:50 -0700

    App: add suffix / to article hrefs

--- client/App/stub.jsx
+++ client/App/stub.jsx
@@ -6,7 +6,7 @@ const ArticleStub = ({ article }) => {
   return (
     <div className="article">
       <h4>
-        <a href={ article.name }>{ article.title }</a>
+        <a href={ `${ article.name }/` }>{ article.title }</a>
         <UTCDate date={ article.date } />
       </h4>
       <div dangerouslySetInnerHTML={ { __html: article.preview } } ></div>
